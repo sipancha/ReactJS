276e6d5554a0db3bd48a2990a9c5a4859ee6d5ff
sipancha <36529672+sipancha@users.noreply.github.com>
2018-07-05 10:04:40 +0530

    author file

---
+++ author.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Author extends React.Component{
+	render() {
+		return (
+			<div className="jumbotron">
+				<div>author</div>
+			</div>
+		);
+	}
+};
+
+export default Author;
